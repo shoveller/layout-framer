6e7b2eafe767b68a8758db9d86f27f3e00955a01
CodeSandbox <support@codesandbox.io>
2023-12-19 15:20:08 +0000

    Sandbox snapshot commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,29 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { motion } from "framer-motion";
 
-function App() {
-  const [count, setCount] = useState(0);
+export default function App() {
+  const [isMaximized, setIsMaximized] = useState(false);
+  const transition = {
+    type: "spring",
+    stiffness: 2000,
+    damping: 100,
+  };
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://reactjs.org" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-      </div>
-      <h1>React + Vite</h1>
-      <h2>On CodeSandbox!</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR.
-        </p>
+    <>
+      <motion.div
+        initial={false}
+        layout
+        transition={transition}
+        className={`wrapper ${isMaximized ? "maximized" : ""}`}
+      >
+        <motion.p transition={transition} layout="position">
+          Hello world
+        </motion.p>
+      </motion.div>
 
-        <p>
-          Tip: you can use the inspector button next to address bar to click on
-          components in the preview and open the code in the editor!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+      <button onClick={() => setIsMaximized(!isMaximized)}>Toggle</button>
+    </>
   );
 }
-
-export default App;
